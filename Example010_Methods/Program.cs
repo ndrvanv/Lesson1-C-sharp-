b85e0dbd62f3873db7f1214373d012cb6b9e9fa8
Andrew <it.andr@outlook.com>
2022-09-19 17:21:56 +0300

    Example 010

---
+++ Example010_Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 24, 43, 4, 58, 58, 6, 75, 78, };
+
+int n = array.Length;
+int find = 58;
+
+int index = 0;     // Элементы в массиве начинаются с 0
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
