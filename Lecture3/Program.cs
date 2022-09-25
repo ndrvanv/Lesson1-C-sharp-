656ef5532e9654f5bee3f28f2adf2583eb2e239d
Andrew <it.andr@outlook.com>
2022-09-25 22:26:06 +0300

    Lecture 3

---
+++ Lecture3/Program.cs
@@ -0,0 +1,146 @@
+﻿// Вид 1 Ничего не возращают и ничего не принимают
+void Method1()
+{
+    Console.WriteLine("Author ...");
+}
+//Method1();
+
+
+// Вид 2 Ничего не возращают, но могут принимать какие либо аргументы
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2(msg: "Текст сообщения"); 
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21(msg: "Текст", count: 4);
+//Method21(count: 4, msg: "Новый текст");
+
+// Вид 3 Что то возращают но ничего не принимают 
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+//int year = Method3();
+//Console.WriteLine(year);
+
+//Вид 4 Что то принимает и что то возращает 
+/*string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}*/
+//string res = Method4(10, "asdf1");
+//Console.WriteLine(res);
+
+
+/*string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "asdf1");
+Console.WriteLine(res);*/
+
+//Цикл внутри цикла
+// Пример вывода таблицы умножения на экран
+
+/*for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+*/
+
+/* 
+Дан текст. В тексте нужно все пробелы заменить черточками, 
+маленькие буквы "к" заменить большими "К", 
+а большие "С" заменить маленькими "с".
+
+Ясна ли задача?*/
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             0123
+// s[3]  --- r
+
+/*string Replace(string text, char oldValue, char newValue )
+{
+    string result = String.Empty;
+    
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);*/
+
+int[] arr = { 4, 1, 2, 3, 6, 5, 7, 9, 5, 1, 7 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = 0; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j; 
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
