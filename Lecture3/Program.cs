75e505ce89e5626cf2125053996ca878e44d7cb2
Andrew <it.andr@outlook.com>
2022-09-25 23:24:43 +0300

    fix a bag

--- Lecture3/Program.cs
+++ Lecture3/Program.cs
@@ -129,7 +129,7 @@ void SelectionSort(int[] array)
     for (int i = 0; i < array.Length - 1; i++)
     {
         int minPosition = i;
-        for (int j = 0; j < array.Length; j++)
+        for (int j = i + 1 ; j < array.Length; j++)
         {
             if (array[j] < array[minPosition]) minPosition = j; 
         }
