21fc0987a0ed4d4c83e3326674b8a9652ae70d95
Andrew <it.andr@outlook.com>
2022-10-03 17:45:56 +0300

    Lec 4.2

---
+++ Lecture4.2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Рекурсия
+
+/*int Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+Console.WriteLine(Factorial(5)); //1*2*3 = 6
+*/
+
+/*double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}"); 
+}
+*/
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)    - Число Фибоначчи
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
