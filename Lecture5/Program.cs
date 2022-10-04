991b35bb8234c7f0702be4f3f6ae0ce4aae960d5
Andrew <it.andr@outlook.com>
2022-10-04 20:18:00 +0300

    Lecture 5 How not to code

---
+++ Lecture5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+// Как не стоит писать код
+/*int a = 12;
+Console.WriteLine(a.GetType().Name);*/
+
+var a = 12;
+//Console.WriteLine(a.GetType());
+
+var data = new int[]{1, 23, 4}
+        .Where(e=> e > 0)
+        .Select(e => new {q = e, w = e + 1});
+Console.WriteLine(data.GetType().Name);   // Убрать Name и будет по другому о необходимости var
