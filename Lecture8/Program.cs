16467f5d21dd0483618286162cd3885b665b21b9
Andrew <it.andr@outlook.com>
2022-10-11 22:18:05 +0300

    Lecture 8

---
+++ Lecture8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using ListMaster;
+
+namespace ConsoleVS;
+
+
+class Program
+{
+    static public void Main(string[] args)
+    {
+        ArrayMaster am = new();
+    }
+}
